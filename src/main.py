baaca69962aba8de5404608c2917681c320b8333
Anirudh C Balaji <anirudh.cb13@gmail.com>
2025-05-11 21:39:13 +0530

    Before running body processing function

--- src/main.py
+++ src/main.py
@@ -7,24 +7,25 @@ from get_header_hough_based import get_header_hough_based
 from get_header_line_based_original import detect_header_with_instructions
 from get_header_fallback import fallback_header_detection 
 from display_image import display_image
-from process_image import process_image
+from process_image import process_image, process_body
 
 
 # Directories
 input_dir = 'images'
-output_dir = 'outputs'
+output_body_dir = 'outputs/body_outputs'
 header_dir = 'headers'
 body_dir = 'bodies'
 split_dir = 'split_images'
 
 # Create output directories if not exist
-for directory in [output_dir, header_dir, body_dir, split_dir]:
+for directory in [output_body_dir, header_dir, body_dir, split_dir]:
     os.makedirs(directory, exist_ok=True)
 
 # Supported image formats
 image_extensions = ('.jpg', '.jpeg', '.png', '.bmp', '.tif', '.tiff')
 already_processed = 0
 newly_processed = 0
+
 # Process each image
 for filename in os.listdir(input_dir):
     if filename.lower().endswith(image_extensions):
@@ -32,7 +33,7 @@ for filename in os.listdir(input_dir):
 
         # Output paths using the same base filename
         base_name = os.path.splitext(filename)[0]
-        output_image_path = os.path.join(output_dir, f"{base_name}_output.jpg")
+        #output_image_path = os.path.join(output_body_dir, f"{base_name}_output.jpg")
         header_path = os.path.join(header_dir, f"{base_name}_header.jpg")
         body_path = os.path.join(body_dir, f"{base_name}_body.jpg")
         split_image_path = split_dir
@@ -59,7 +60,36 @@ for filename in os.listdir(input_dir):
 
 print (f"{already_processed} files left untouched, {newly_processed} files processed")
 
-visualize_header_detection('images/sample8.jpg')
+bodies_already_processed = 0
+bodies_newly_processed =0
+for filename in os.listdir(body_dir):
+    if filename.lower().endswith(image_extensions):
+        image_path = os.path.join(body_dir, filename)
+
+        # Output paths using the same base filename
+        base_name = os.path.splitext(filename)[0]
+        output_image_path = os.path.join(output_body_dir, f"{base_name}_output.jpg")
+        
+        # Skip if output already exists
+        if (os.path.exists(output_image_path)) :
+            print(f"✅ Skipping {filename} (already processed).")
+            bodies_already_processed = bodies_already_processed +1
+            continue
+
+        try:
+            print(f"\n🚀 Processing {filename}...")
+            process_body(image_path, output_image_path)
+            bodies_newly_processed = bodies_newly_processed +1
+            # Optional: display the result visually (can be commented out for batch runs)
+            #display_image(header, f"{filename} - header")
+            #display_image(body, f"{filename} - body")
+
+        except Exception as e:
+            print(f"❌ Error processing {filename}: {e}")
+
+print (f"{bodies_already_processed} body file(s) left untouched, {newly_processed} body file(s) processed")
+
+#visualize_header_detection('images/sample8.jpg')
 
 """
 #MAIN
--- src/process_image.py
+++ src/process_image.py
@@ -8,6 +8,61 @@ from display_image import display_image
 import os
 
 
+
+def process_body(image_path, output_path):
+    if not os.path.exists(image_path):
+        raise FileNotFoundError(f"Image not found: {image_path}")
+    
+    img = cv2.imread(image_path)
+    
+    # Check if image is loaded successfully
+    if img is None:
+        print(f"Error: Could not read the image at {image_path}")
+        return
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #display_image(gray, "step 1 - gray")
+
+
+    #finding header and body distinction
+    #header,body = get_header_threshold_based(img, gray)
+    #display_image(header, "Header")
+    #display_image(body, "body")
+
+    # Apply adaptive thresholding
+    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                   cv2.THRESH_BINARY_INV, 15, 10)
+    
+    #display_image(thresh, "step 2 - after thresh")
+    
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 5))
+    dilated = cv2.dilate(thresh, kernel, iterations=1)
+
+    #display_image(dilated, "step 3 - dilated")
+    # Find contours (connected components)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #display_image(contours, "step 4 - contours")
+
+    # List to store bounding boxes
+    boxes = []
+
+    # Loop through contours to get bounding boxes
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        #print(f"Box: x={x}, y={y}, w={w}, h={h}")
+        if ( w > 20 and h > 20):  # Filter out noise based on box size (adjust as needed)
+            boxes.append((x, y, w, h))
+
+    # Draw bounding boxes on the original image
+    for x, y, w, h in boxes:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    
+    # Save the output image with bounding boxes
+    display_image(img)
+    cv2.imwrite(output_path, img)
+    print(f"Bounding boxes drawn and saved to {output_path}")
+
 # Function to process image and find bounding boxes
 def process_image(image_path, output_path):
     # Read the image
